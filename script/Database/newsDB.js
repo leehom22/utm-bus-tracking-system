5d180b6392aaf6cfab0d342dda00972cde5aebeb
Ling Lee Hom <102334679+leehom22@users.noreply.github.com>
2024-11-17 13:42:15 +0800

    feedback and news data

---
+++ script/Database/newsDB.js
@@ -0,0 +1,16 @@
+const news=[{
+  title:'UTM Shutle Bus will be provided during convo week',
+  date:'6 November 2024',
+  time:'4:29pm',
+  content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+},{
+  title:'UTM Bus route CP-Stadium',
+  date:'11 November 2024',
+  time:'10:44am',
+  content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+},{
+  title:'Lingkaran Ilmu road maintenance',
+  date:'17 November 2024',
+  time:'12:34pm',
+  content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+}]
