251a0bddecdd070da5587925593238af38f99bf5
Ling Lee Hom <102334679+leehom22@users.noreply.github.com>
2024-12-05 12:08:28 +0800

    Update firestore.js

--- client-side/script/firestore.js
+++ client-side/script/firestore.js
@@ -8,13 +8,7 @@ import { busStop_G } from "./data/scheduleDB.js";
 
 //setLogLevel("debug");
   const firebaseConfig = {
-    apiKey: "AIzaSyCboB1zkwOfNWQkkRqi8l9A9HoNhMBHTgY",
-    authDomain: "lofty-chemist-442412-f9.firebaseapp.com",
-    projectId: "lofty-chemist-442412-f9",
-    storageBucket: "lofty-chemist-442412-f9.firebasestorage.app",
-    messagingSenderId: "672389647219",
-    appId: "1:672389647219:web:391489d3967ea780edd306",
-    measurementId: "G-GY87RY7G96"
+//firebaseConfig
   };
  
 
